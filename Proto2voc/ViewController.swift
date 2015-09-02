e308e64ac795c3fe7f685f3ec039341e183d6999
Martijn van Gogh <martijnvangogh@MacBook-Pro-van-Martijn.local>
2015-09-02 15:28:15 +0200

    Initial Commit

---
+++ Proto2voc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proto2voc
+//
+//  Created by Martijn van Gogh on 02-09-15.
+//  Copyright (c) 2015 Martijn van Gogh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
